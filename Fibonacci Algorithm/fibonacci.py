38a2da4280822adcf0c4e4023f80c82906aa653a
Farid <155392795+Digitalturkk@users.noreply.github.com>
2024-08-13 18:35:58 +0400

    Input addition

--- Fibonacci Algorithm/fibonacci.py
+++ Fibonacci Algorithm/fibonacci.py
@@ -4,4 +4,8 @@ def fibonacci(x, y, z):
         print(y)
         x, y = y, x + y
 
-fibonacci(1, 2, 10) # Any number of arguments can be passed
+number_one = int(input("Enter the first number: "))
+number_two = int(input("Enter the second number: "))
+number_of_terms = int(input("Enter the number of terms: "))
+
+fibonacci(number_one, number_two, number_of_terms)
