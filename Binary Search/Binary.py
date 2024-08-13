6db5f6f785a79a1ef8114be56f0313d0fe1376b5
Farid <155392795+Digitalturkk@users.noreply.github.com>
2024-08-14 00:54:16 +0400

    The python code for binary search

--- Binary Search/Binary.py
+++ Binary Search/Binary.py
@@ -1,1 +1,16 @@
+def search(arr, item):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (high + low) // 2
+        if arr[mid] == item:
+            return ("The item is at index ", mid, "The item is ", arr[mid])
+        if arr[mid] < item:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return "The item is not in the list"
 
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+item = int(input("Enter the item you want to search for: "))
+print(search(arr, item))
