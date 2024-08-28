af908cafdca141225bffbb4eb1f12d5e75da936c
Farid <155392795+Digitalturkk@users.noreply.github.com>
2024-08-29 01:21:16 +0400

    The code (java)

--- Finde Max Value/FindMax.java
+++ Finde Max Value/FindMax.java
@@ -1,1 +1,17 @@
+class FindMax {
+    public static void main(String[] args) {
+        int[] list = {15, 10, 12, 33, 56, 22};
 
+        int index = 0; 
+        int max = list[index];
+
+        for (int i : list) {
+            if (i > max) {
+                max = list[index];      
+            }
+
+            index++;
+        }
+        System.out.printf("The max value of our list is %d", max);
+    }
+}
