a08450276988a6c4843dd098a4693961938f7b64
Farid <155392795+Digitalturkk@users.noreply.github.com>
2024-08-29 01:50:51 +0400

    code for FindMax2.java
    Same ass FindMax.java, but added the index finder using binary search

--- Finde Max Value/FindMax2.java
+++ Finde Max Value/FindMax2.java
@@ -1,1 +1,40 @@
+class FindMax {
 
+    static int findIndex(int[] arr, int value) {
+        
+        int low = 0;
+        int high = arr.length - 1;
+ 
+        while (low <= high) {
+
+            int mid = (high+low)/2;
+
+            if (arr[mid] == value){
+                return mid;
+            } else if (arr[mid] > value) {
+                high = mid;
+            } else if (arr[mid] < value){
+                low = mid;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] list = {15, 10, 12, 33, 56, 22};
+
+        int index = 0; 
+        int max = list[index];
+
+        for (int i : list) {
+            if (i > max) {
+                max = list[index];      
+            }
+            index++;
+        }
+        
+        index = findIndex(list, max);
+        System.out.printf("The max value of our list is %d, at index %d", max, index);
+    }
+}
