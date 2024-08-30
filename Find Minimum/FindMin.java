e466a8502c72fdc215c3cd3bbbab59a13b7345d9
Farid <155392795+Digitalturkk@users.noreply.github.com>
2024-08-31 01:28:19 +0400

    The Code

--- Find Minimum/FindMin.java
+++ Find Minimum/FindMin.java
@@ -1,1 +1,19 @@
+public class FindMin {
+    public static int min(int[] arr) {
+        int target = arr[0];
+        int index = 0;
 
+        for (int i : arr) {
+            if (i < target) {
+                target = arr[index];
+            }
+            index++;
+        }
+        return target;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {13, 45, 56, 78, -20, -10};
+        System.out.println(min(array));
+    }
+}
