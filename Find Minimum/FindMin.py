42ba5dc3d19ad4783983e93de141421c116c8ba4
Farid <155392795+Digitalturkk@users.noreply.github.com>
2024-09-01 00:05:25 +0400

    The python code

--- Find Minimum/FindMin.py
+++ Find Minimum/FindMin.py
@@ -1,1 +1,9 @@
+def find_min(list1):
+    min = list1[0]
+    for i in list1:
+        if i < min:
+            min = i
+    return min
 
+list_one = [13, 44, 576, 87, -12, 0, -45, 78]
+print(find_min(list_one))
