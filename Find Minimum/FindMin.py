9e494a098a61c556722262a9f7f8716e576d83d7
Farid <155392795+Digitalturkk@users.noreply.github.com>
2024-09-03 12:42:16 +0400

    # comment

--- Find Minimum/FindMin.py
+++ Find Minimum/FindMin.py
@@ -5,5 +5,5 @@ def find_min(list1):
             min = i
     return min
 
-list_one = [13, 44, 576, 87, -12, 0, -45, 78]
+list_one = [13, 44, 576, 87, -12, 0, -45, 78] # Write your list
 print(find_min(list_one))
