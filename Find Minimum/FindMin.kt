56b7e4a83245587aca082458ca3add0e6ca23ee4
ITtsuki <155392795+Digitalturkk@users.noreply.github.com>
2025-11-26 00:57:52 +0400

    Implement findMin function to find minimum value

--- Find Minimum/FindMin.kt
+++ Find Minimum/FindMin.kt
@@ -1,3 +1,13 @@
+fun findMin(arr: IntArray): Int {
+    var min = arr[0]
+    for (i in arr) {
+        if (i < min) {
+            min = i
+        }
+    }
+    return min
+}
 fun main() {
-  
+    val array = intArrayOf(13, 45, 56, 78, -20, -10)
+    println(findMin(array))
 }
