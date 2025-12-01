0dddfbbfa9f74dd862d4c6548c8c0aae68255b17
ITtsuki <155392795+Digitalturkk@users.noreply.github.com>
2025-12-02 01:03:36 +0400

    Implement function to find maximum value in array

--- Finde Max Value/findMax.kt
+++ Finde Max Value/findMax.kt
@@ -1,2 +1,14 @@
-fun() main {
+fun finMax(arr: IntArray): Int {
+    var max: Int = arr[0]
+    for (i in arr) {
+        if (i > max) {
+            max = i
+        }
+    }
+    return max
+}
+
+fun main() {
+    val array = intArrayOf(5, 3, 8, 1, 4, -9, 10)
+    println("Max value is: ${finMax(array)}")
 }
