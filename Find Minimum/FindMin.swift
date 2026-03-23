7626f64787c105ac25fdf9ff651878d18aa1f309
ITtsuki <155392795+Digitalturkk@users.noreply.github.com>
2026-03-24 01:17:36 +0400

    Create findMin function to find the minimum value
    Refactored minimum finding logic into a function.

--- Find Minimum/FindMin.swift
+++ Find Minimum/FindMin.swift
@@ -1,11 +1,16 @@
 import UIKit
 
+func findMin(arr:[Int]) {
+    var lowest = arr[0]
+    
+    for i in arr {
+        if i < lowest {
+            lowest = i
+        }
+    }
+    print(lowest)
+}
+
 let arr = [23,12,233,-1,0,33,2,-22,345]
 
-var lowest = arr[0]
-
-for i in arr {
-    if i < lowest {
-        lowest = i
-    }
-}
+findMin(arr: arr)
