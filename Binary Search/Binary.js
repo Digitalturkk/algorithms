9a04b6ef5e9533d62d8401e9d42d0a805532d52b
ITtsuki <155392795+Digitalturkk@users.noreply.github.com>
2026-04-09 03:01:44 +0400

    Implement binary search algorithm in Binary.js

--- Binary Search/Binary.js
+++ Binary Search/Binary.js
@@ -1,1 +1,19 @@
+function binarySearch(arr, target) {
+    let left = 0
+    let right = arr.length - 1
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2)
+        if (arr[mid] === target) {
+            return mid
+        } else if (arr[mid] > target) {
+            right = mid - 1
+        } else {
+            left = mid + 1
+        }
+    }
+    return -1
+}
 
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+let target = 20
+console.log(binarySearch(arr, target))
